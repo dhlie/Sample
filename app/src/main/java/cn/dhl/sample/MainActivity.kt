997646fd57e77d3ebacb815c07d3c6060e0ecb4b
DuanHl <dhlie@163.com>
2021-05-15 14:12:39 +0800

    permission

--- app/src/main/java/cn/dhl/sample/MainActivity.kt
+++ app/src/main/java/cn/dhl/sample/MainActivity.kt
@@ -1,9 +1,11 @@
 package cn.dhl.sample
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import cn.dhl.sample.dagger.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import cn.dhl.sample.dagger.DaggerActivity
 import cn.dhl.sample.databinding.ActivityMainBinding
 import cn.dhl.sample.popup.PopupActivity
 
@@ -18,6 +20,53 @@ class MainActivity : AppCompatActivity() {
 
         binding.btn1.setOnClickListener { startActivity(Intent(applicationContext, DaggerActivity::class.java)) }
         binding.btn2.setOnClickListener { startActivity(Intent(applicationContext, PopupActivity::class.java)) }
+        binding.btn3.setOnClickListener { requestPermission() }
 
     }
+
+    private fun requestPermission() {
+        PermissionHelper.with(this)
+            .permission(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            .rationale { perms, consumer ->
+                val ps = StringBuilder()
+                for (p in perms) {
+                    ps.append("\n").append(PermissionHelper.permissionToText(p))
+                }
+
+                AlertDialog.Builder(this)
+                    .setMessage("需要以下权限才能执行此操作:$ps")
+                    .setPositiveButton("确定") { _, _ ->
+                        consumer.accept()
+                    }
+                    .setNegativeButton("取消") { _, _ ->
+                        consumer.deny()
+                    }
+                    .show()
+            }
+            .onAllGranted {
+            }
+            .onDenied { perms ->
+            }
+            .onNoAskAgain { perms ->
+                val ps = StringBuilder()
+                for (p in perms) {
+                    ps.append("\n").append(PermissionHelper.permissionToText(p))
+                }
+
+                AlertDialog.Builder(this)
+                    .setMessage("请到设置页面开启如下权限:$ps")
+                    .setPositiveButton("确定") { _, _ ->
+                        PermissionHelper.toAppSetting(this)
+                    }
+                    .setNegativeButton("取消", null)
+                    .show()
+            }
+            .start()
+    }
+
 }
